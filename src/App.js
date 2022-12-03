93a76b141f5996e7304e3ddd6785e3013ec3cf00
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2022-12-03 16:21:30 +0530

    Adding and styling components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="section">
+        <form>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search For Something"
+          />
+          <button>Search</button>
+        </form>
+
+        <article>
+          <h1>Very big title here</h1>
+          <a href="">Read Full Article</a>
+        </article>
+
+        <article>
+          <div>
+            <h2>Heading 2</h2>
+            <ul>
+              <li>By Ashish</li>
+              <li>
+                <a href="">Read Full Article</a>
+              </li>
+            </ul>
+            <p>Date</p>
+          </div>
+        </article>
+      </section>
+    </>
   );
 }
 
